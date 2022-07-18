8bc90b5087de45c992a4015ba947ddb2361e271a
s981672 <sungyol@gmail.com>
2022-07-18 15:26:43 +0900

    임시로 ID/PWD 막음

--- app/database/mongodb_handler.py
+++ app/database/mongodb_handler.py
@@ -12,7 +12,7 @@ class MongoDBHandler(DBHandler):
     def __init__(self, host="localhost/", db_name=None, collection_name=None):
         username = urllib.parse.quote_plus('bigmoneylab')
         password = urllib.parse.quote_plus('Wjswlgus2@22')
-        self._db_client = MongoClient(f"mongodb://{username}:{password}@{host}", port=27027)
+        self._db_client = MongoClient(f"mongodb://{host}", port=27027)
         
         if db_name is not None:
             self._db = self._db_client[db_name]
