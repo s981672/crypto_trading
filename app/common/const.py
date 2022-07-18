f7524b2a2e558b1738e411c71c5808f903cb43ff
s981672 <sungyol@gmail.com>
2022-07-18 14:46:27 +0900

    DB port 번호 int로 변경

--- app/common/const.py
+++ app/common/const.py
@@ -1,7 +1,7 @@
 
 class DBConst:
     DB_NAME = "bml"
-    DB_PORT = 27027
+    DB_PORT: int = 27027
     USER_COLLECTION_NAME = 'user'
     API_KEY_COLLECTION_NAME = 'api_key'
     STRATEGY_COLLECTION_NAME = 'strategy'
