c626b54ee73e7aeaf9e19574226d1eb55e8677e3
s981672 <sungyol@gmail.com>
2022-07-29 11:45:12 +0900

    DB 로그 추가

--- app/database/mariadb_handler.py
+++ app/database/mariadb_handler.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any, List
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -6,6 +7,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from database.base_handler import DBHandler
 
 from error.error import InvalidParamError
+
+logger = logging.getLogger('sLogger')
 
 class MariadbHandler(DBHandler):
     def __init__(self, hostName:str = "127.0.0.1"):
@@ -29,6 +32,7 @@ class MariadbHandler(DBHandler):
         
         session = self.__get_session()
         
+        logger.info(f"[DB] INSERT ITEM : {item}")
         session.add(item)
         session.commit()
 
@@ -38,15 +42,20 @@ class MariadbHandler(DBHandler):
         
         session = self.__get_session()
         
+        logger.info(f"[DB] INSERT ITEMS : {items}")
         session.add_all(items)
         session.commit()
     
     def find_item(self, entities, condition=None):
+        logger.info(f"[DB] FIND ITEM. condtion :{condition}")
+
         query_result = self.find_items(entities, condition)
         if len(query_result) > 0 :
             return query_result[0]
     
     def find_items(self, entities, condition=None):
+        logger.info(f"[DB] FIND ITEMs. condtion :{condition}")
+
         if condition is None:
             condition = {}
             
@@ -57,6 +66,8 @@ class MariadbHandler(DBHandler):
         return query_result
         
     def delete_item(self, item):
+        logger.info(f"[DB] DELETE ITEM : {item}")
+
         if item is None:
             raise Exception()
         
@@ -66,6 +77,8 @@ class MariadbHandler(DBHandler):
     
         
     def update_item(self, condition=None, item=None):
+        logger.info(f"[DB] UPDATE ITEM. condition:{condition}, item: {item}")
+
         if condition is None or item is None:
             raise Exception()
         
