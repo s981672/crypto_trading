55af6ca6c51aab500aca01e9efcbdd38bb69a13e
s981672 <sungyol@gmail.com>
2022-08-02 09:59:27 +0900

    Update trading_view_event.py
    order_id가 없는 경우의 예외 처리 추가

--- app/models/trading_view_event.py
+++ app/models/trading_view_event.py
@@ -18,6 +18,9 @@ class TradingViewEvent(BaseModel):
         # order id 값이 'Close Entry(s) order L[1]' 형태로 올 수도 있기에 정규식을 사용하여 ID를 추출함.
         regex = re.compile(r'[LS]\[[0-9]\]')
         order_id = regex.search(data['order_id'])
-        data['order_id'] = order_id.group()
-
+        if order_id != None:
+            data['order_id'] = order_id.group()
+        else:
+            data['order_id'].split()[-1]
         super().__init__(**data)
+        
